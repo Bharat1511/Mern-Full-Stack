b73f30c0093dd30c4f18d8c85547945ef3bcf522
bharat1511 <bharatsahu15112004@gmail.com>
2025-11-17 17:56:02 +0530

    complete

---
+++ JavaScript/BasicJS/index.js
@@ -0,0 +1,27 @@
+var a = 10;
+
+var a = 20;
+
+let b = 20;
+
+
+console.log(a, b);
+
+
+let ar = ['abc', 1234, 24.76, "a", true]
+console.log(ar);
+
+console.warn("this is warning");
+
+console.error("ye error hai");
+
+alert("this is alert")
+
+
+if (confirm("Are you sure")){
+    console.log("thanks")
+}
+else {
+    console.error("It's ok")
+}
+
