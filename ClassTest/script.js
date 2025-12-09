c263f6f21a6d787b13c82c58cdf5eb75729b1568
Bharat1511 <bharatsahu15112004@gmail.com>
2025-12-09 08:13:36 +0530

    test completed

---
+++ ClassTest/script.js
@@ -0,0 +1,12 @@
+submit(){
+
+    console.log("Form Submitted");   
+    const em = document.getElementById("email").value;
+
+    const msg = document.getElementById("eessage").value;           
+    console.log("Email: " + em);
+    console.log("Message: " + msg);
+
+    alert("Form Submitted Successfully");
+
+}
