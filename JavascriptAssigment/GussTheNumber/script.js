73a1b5a422894209bc55a380cf6af6bbc86908eb
Bharat1511 <bharatsahu15112004@gmail.com>
2025-12-22 14:56:43 +0530

    assigment

--- JavascriptAssigment/GussTheNumber/script.js
+++ JavascriptAssigment/GussTheNumber/script.js
@@ -1,4 +1,29 @@
-function checkguess() {
+
+function checkGuess() {
+
+    const randomno = Math.floor(Math.random() * 9)+1;
+    const guessno = document.getElementById("guessno").value;
+    const display = document.getElementById("display");
+
+    if(!guessno)
+    {
+        alert("please Enter the No.")
+        return;
+    }
+
+    if(guessno>randomno)
+    {
+        display.innerText =  "OOPS! SORRY!!! TRY A SMALLER NUMBER.";
+    }
+
+    else if(guessno<randomno)
+    {
+        display.innerText = "OOPS! SORRY!!! TRY A LARGER NUMBER.";
+    }
+    else{
+        display.innerText= "Congratulation!!! you Guessed correct No."
+    }
+
   
 
 }
