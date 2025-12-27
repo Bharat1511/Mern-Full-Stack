54ccb87122b7a6a4fefff82b9d6e3dfd17b6a9e9
Bharat1511 <bharatsahu15112004@gmail.com>
2025-12-27 14:15:50 +0530

    react App website

--- React/Demo/src/App.jsx
+++ React/Demo/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+import { BrowserRouter,Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Product from "./pages/Product";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  // let a = 5;
+  // let b = 10;
+  // console.log(a+b);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <h1 id="head">
+        <i className="bi bi-airplane-engines-fill"></i> Hello World
+      </h1> */}
+
+
+      {/* { <div className="bg-primary text-light">This is my first React App</div>
+      <Header />
+      <Home/> }
+      <Footer/> */}
+
+
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/product" element={<Product/>} />
+      <Route path="/contact" element={<Contact/>} />
+       </Routes>
+
+
+   <Footer/>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ React/Demo/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+const Home = () => {
+  return (
+    <>
+     <main>
+        
+            <div class="" id="hero">
+                <h1 className="text-center mt-4">Welcome to Shopping Kart</h1>
+                <p className="fs-0 p-3"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellat fugit mollitia itaque. Cum, sint. Expedita, unde esse inventore asperiores modi ex error nesciunt nostrum enim suscipit, adipisci aut maxime.
+                Enim, sed adipisci, quam possimus provident similiqu Consequatur aut officia possimus iste temporibus laudantium, ex, animi odio quas labore maxime provident unde hic consectetur harum tenetur eveniet quaerat nulla quis debitis nostrum? Delectus dolorem blanditiis est sunt.
+                Cum, commodi libero! Laudantium sunt commodi explicabo fuga harum veritatis delectus, quasi iste tempore? Perspiciatis ipsum aspernatur eligendi quis! Dolorum non magnam dolores! Esse, et magnam. Voluptates vitae illum quisquam.
+                Vel nesciunt alias aliquid eaque cupiditate asperiores quo totam, iste labore, vero itaque quasi modi laudantium enim explicabo dignissimos perferendis officiis maxime placeat? Quidem dicta voluptas laudantium nihil minus enim.Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde libero ad ipsa maiores obcaecati vel, dolorum ullam, exercitationem asperiores sed reiciendis, ex temporibus laboriosam facere eligendi quidem assumenda quod quasi. consectetur adipisicing elit. Nostrum voluptatem molestiae voluptates obcaecati repellendus qui nisi fuga tenetur consequatur nesciunt pariatur atque reprehenderit in, ullam reiciendis maxime, dolorum earum blanditiis. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque, a rerum. Veritatis odit repellendus culpa assumenda corporis, recusandae aut quia fugiat consequatur cumque hic quidem. Voluptatem veritatis eius unde omnis?</p>
+            </div>
+            
+            <div className="mx-4 w-25 h-25">
+                <a href="/product"><button id="btn" className="btn btn-dark btn-lg">Start Shopping <i className="bi bi-arrow-right-circle-fill"></i></button></a>
+            </div>
+    </main>
+    </>
+  );
+};
+
+export default Home;
